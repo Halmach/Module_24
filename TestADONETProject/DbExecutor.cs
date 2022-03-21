01c4031a2097d4841f692b46bbc682b759e496f5
Khalmatov <large000@yandex.ru>
2022-03-21 22:20:18 +0300

    Задание 24.2.8 Запишите в переменную selectcommandtext текст инструкции SQL на выборку всех данных и добавьте имя таблицы из переменной table.

---
+++ TestADONETProject/DbExecutor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace TestADONETProject
+{
+    public class DbExecutor
+    {
+        private MainConnector connector;
+        public DbExecutor(MainConnector connector)
+        {
+            this.connector = connector;
+        }
+
+        public DataTable SelectAll(string table)
+        {
+            var selectcommandtext = "select * from " + table;
+            var adapter = new SqlDataAdapter(selectcommandtext, connector.GetConnection());
+        }
+    }
+}
